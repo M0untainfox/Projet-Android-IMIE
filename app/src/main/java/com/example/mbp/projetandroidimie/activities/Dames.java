b2c0c97b9896fb4cfe47aeb7fc3ce776994437cd
M0untainfox <mbp@MacBook-Pro-de-Antoine.local>
2017-10-04 10:05:43 +0200

    Adding a layout for Draught 🦊
    🦊🦊🦊

--- app/src/main/java/com/example/mbp/projetandroidimie/activities/Dames.java
+++ app/src/main/java/com/example/mbp/projetandroidimie/activities/Dames.java
@@ -16,7 +16,7 @@ public class Dames extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.battleship_layout);
+        setContentView(R.layout.dames_laytout);
 
 
 
